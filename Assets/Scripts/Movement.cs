9b32977764bde4dce8f64eb758eac675a48bdc14
Tasha Hankewych <thankewych@gmail.com>
2025-06-03 16:16:23 -0500

    Add an input action to rotate the rocket

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,10 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+
     [SerializeField] float thrustStrength = 1000f;
+    [SerializeField] float rotationStrength = 100f;
 
     Rigidbody rb;
 
@@ -16,13 +19,39 @@ public class Movement : MonoBehaviour
     void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+
+        if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+        else if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
